c71de6d705bfb4b567344c2adf378e935d931c97
nanangqq <gingggg@gmail.com>
2021-02-12 12:12:53 +0000

    done

---
+++ dict/dict.go
@@ -0,0 +1,53 @@
+package dict
+
+import "errors"
+
+// Dictionary type
+type Dictionary map[string]string
+
+var errNotFound = errors.New("not found")
+var errWordExists = errors.New("word exists")
+var errCantUpdate = errors.New("cant update not-existing word")
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	// if err == errNotFound {
+	// 	d[word] = def
+	// } else if err == nil {
+	// 	return errWordExists
+	// }
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"gogo3/dict"
+)
+
+func main() {
+	dictionary := dict.Dictionary{}
+	// dictionary["hello"] = "hello"
+	fmt.Println(dictionary)
+
+	def, err := dictionary.Search("hi")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(def)
+	}
+
+	err = dictionary.Add("hello", "hi")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	def, err = dictionary.Search("hello")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(def)
+	}
+	fmt.Println(dictionary)
+
+	baseWord := "test"
+	dictionary.Add(baseWord, "first")
+	err = dictionary.Update(baseWord, "second")
+	if err != nil {
+		fmt.Println(err)
+	}
+	value, _ := dictionary.Search(baseWord)
+	fmt.Println(value)
+
+	dictionary.Delete(baseWord)
+	fmt.Println(dictionary)
+}
